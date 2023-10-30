373f325d6adb0692dc87a3185645a3c00b2dad29
Arpit <arpitjaiswal9526@gmail.com>
2023-10-30 18:53:49 +0530

    First commit currently in Development

---
+++ seendeemmail/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SeendeemmailConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'seendeemmail'
---
+++ seendeemmail/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def main(requests):
+    return render(requests, "index.html")
