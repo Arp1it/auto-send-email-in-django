fd31ff29726f49d82a41b30a11072a876fb48df5
Arpit <arpitjaiswal9526@gmail.com>
2023-10-31 10:39:24 +0530

    Currently in Development

--- seendeemmail/models.py
+++ seendeemmail/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
 
 # Create your models here.
+class MailSender(models.Model):
+    cuser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="mailuser")
+    senderemail = models.EmailField(max_length=50)
+    senderappemailpass = models.CharField(max_length=50)
+    senderemail = models.EmailField(max_length=50)
+    sendingdate = models.CharField(default=str(now), max_length=30)
+    timestamp = models.DateTimeField(default=now)
--- seendeemmail/views.py
+++ seendeemmail/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def main(requests):
     return render(requests, "index.html")
+
+
+def fetching(requests):
+    pass
